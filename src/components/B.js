d09a85d6c0ffde9f9f38c9bb1c7dbae54366ec39
최오늘 <healutoday@naver.com>
2024-05-15 21:12:33 +0900

    수정

--- src/components/B.js
+++ src/components/B.js
@@ -1,33 +1,33 @@
-import React from 'react'
-
+import React from "react";
+//React.memo의 사용 감싸주면 된다.
 const Message = React.memo(({ message }) => {
   return <p> {message} </p>;
-})
+});
 
 const ListItem = React.memo(({ posts }) => {
   return (
     <li key={posts.id}>
-       <p>{posts.title}</p>
+      <p>{posts.title}</p>
     </li>
   );
 });
 
 const List = React.memo(({ posts }) => {
-      return(
-        <ul>
-          {posts.map(posts => (
-                <ListItem key={posts.id} posts={posts} />
-          ))}
-        </ul>
-      );
+  return (
+    <ul>
+      {posts.map((posts) => (
+        <ListItem key={posts.id} posts={posts} />
+      ))}
+    </ul>
+  );
 });
 
 const B = ({ message, posts }) => {
   return (
     <div>
-       <h1>B component</h1>
-       <Message message={message} />
-       <List posts={posts} />
+      <h1>B component</h1>
+      <Message message={message} />
+      <List posts={posts} />
     </div>
   );
 };
